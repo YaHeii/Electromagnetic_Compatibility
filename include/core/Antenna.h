008f18fb52d24db16013d78c78b69274a2111384
YaHeii <c15146681278@163.com>
2025-12-10 15:41:57 +0800

    a

--- include/core/Antenna.h
+++ include/core/Antenna.h
@@ -2,8 +2,11 @@
 #include <string>
 
 /// <summary>
-/// 
-/// 
+/// @param id 天线标识符
+/// @param relative_height 天线相对高度（相对于地面或海平面）
+/// @param beam_width_deg 天线波束宽度（度）
+/// @param tilt_deg 天线倾斜角（度），默认为0
+/// @brief 天线基类
 /// </summary>
 class Antenna {
 public:
@@ -17,7 +20,7 @@ public:
 	double getBeamWidthDeg() const { return _beam_width_deg; }
 	double getTiltDeg() const { return _tilt_deg; }
 
-protected:
+private:
     std::string _id;
     double _relative_height;
     double _beam_width_deg;
@@ -27,27 +30,28 @@ protected:
 // 一个简单的全向天线
 class OmniAntenna : public Antenna {
 public:
+    //弃用
     OmniAntenna(const std::string& id, double gain_dbi = 0.0)
-        : Antenna(id), m_gain_dbi(gain_dbi) {}
-
-    double getGainDbi(double azimuth_deg_relative_to_antenna) const override {
-        return m_gain_dbi; // 任何方向增益都一样
+        : Antenna(id, 0.0, 360.0, 0.0),
+        _gain_dbi(gain_dbi) {}
+    double getGainDbi(double azimuth_deg_relative_to_antenna) const {
+        return _gain_dbi; // 任何方向增益都一样
     }
 private:
-    double m_gain_dbi;
+    double _gain_dbi;
 };
 
 //定向天线
 class DirectionalAntenna : public Antenna {
 public:
-    DirectionalAntenna(const std::string& id, double gain_dbi = 0.0, double azimuth_deg = 0.0, double elevation_deg = 0.0)//天线朝向为azimuth_deg, 仰角为elevation_deg, 增益为gain_dbi
-        : Antenna(id, azimuth_deg), m_gain_dbi(gain_dbi), m_azimuth_deg(azimuth_deg), m_elevation_deg(elevation_deg) {}
-
-    double getGainDbi(double azimuth_deg_relative_to_antenna) const override {
-        return m_gain_dbi; // 任何方向增益都一样
+    // 弃用
+    DirectionalAntenna(const std::string& id, double gain_dbi = 0.0, double azimuth_deg = 0.0, double tilt_deg = 0.0)//天线朝向为azimuth_deg, 仰角为elevation_deg, 增益为gain_dbi
+        : Antenna(id, 0.0, 360.0, tilt_deg),  _gain_dbi(gain_dbi), _azimuth_deg(azimuth_deg) {}
+    //TODO：弃用，使用垂直场分布u(0,z)
+    double getGainDbi(double azimuth_deg_relative_to_antenna) const {
+        return _gain_dbi; // 任何方向增益都一样
     }
 private:
-    double m_gain_dbi;
-    double m_azimuth_deg;
-    double m_elevation_deg;
+    double _gain_dbi;
+    double _azimuth_deg;
 };
